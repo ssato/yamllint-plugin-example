78f317648c7bf9e82240970e2f7830d8dc759143
Satoru SATOH <satoru.satoh@gmail.com>
2020-09-24 00:46:56 +0900

    fix: suppress some pylint warnings

--- tests/common.py
+++ tests/common.py
@@ -12,6 +12,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# pylint: disable=missing-module-docstring,missing-class-docstring,
+# pylint: disable=missing-function-docstring
 
 import os
 import tempfile
